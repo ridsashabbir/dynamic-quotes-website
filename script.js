927da70cef141420ea35e12690fcdae1fcfaf133
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-09-01 00:55:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+let realData = "";
 const getQuotes = async () => {
   const api = "https://type.fit/api/quotes";
   try {
     let data = await fetch(api);
-    let realData = await data.json();
-    console.log(realData);
-    console.log(realData[0]);
+    realData = await data.json();
+    // console.log(realData);
+    // console.log(realData[0]);
+    console.log(realData[0].length);
     console.log(realData[0].text);
+    console.log(realData[0].author);
   } catch (error) {}
 };
 
