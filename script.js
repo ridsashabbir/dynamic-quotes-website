ba885ce5245bfddd4117b5fa8f236dfa3bbf875e
ridsa00 <81819648+ridsashabbir@users.noreply.github.com>
2023-09-04 19:37:05 +0500

    ..

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let realData = "";
-const 
+const quotes = document.getElementById("quotes");
+const author = document.getElementById("author");
 
 const getNewQuotes = () => {
   let rnum = Math.floor(Math.random() * 10);
   console.log(rnum);
   console.log(realData[rnum].author);
+  quotes.innerText = `${realData[rnum].text}`;
 };
 
 const getQuotes = async () => {
